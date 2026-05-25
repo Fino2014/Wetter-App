5a3174797b369caa1ab0e750f78a2cc57f2f94d1
Natacha <marthenatacha.megnegne@studmail.w-hs.de>
2026-05-25 16:20:18 +0200

    Frontend Grundgerüst mit CSS und JS erstellt

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('wetter-btn');
+const output = document.getElementById('wetter-output');
+
+button.addEventListener('click', () => {
+    output.innerText = "Lade Wetterdaten... (Hier kommt bald eure KI!)";
+});
